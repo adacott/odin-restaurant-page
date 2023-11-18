4158db535a0111df2fe8fff1f47dab723f803e37
Adam <adamryancottrill@gmail.com>
2023-11-18 15:00:12 -0500

    Adding initial function to build out page

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,28 @@ console.log("Connected and watching");
 // This will create and setup the header, main, and footer elements for manipulation by the rest of the modules
 // In this way, on load, the page will dynamically add the content for the header, main, and footer sections, then 
 // createHome will simply redo all of the initial setup to take you back to the home page when the link is clicked
+window.onload = function () {
+    const content = document.getElementById("content");
+    const home = document.createElement("div");
+    const header = document.createElement("header");
+    const main = document.createElement("main");
+    const footer = document.createElement("footer");
+    content.appendChild(home);
+    home.id = "home";
+    home.appendChild(header);
+    home.appendChild(main);
+    home.appendChild(footer);
+
+    // Elements for the creation of the header:
+    const div = document.createElement("div");
+    const im = document.createElement("img");
+    const title = document.createElement("h1");
+    title.textContent = "Adam's Pizza Shack";
+
+    div.appendChild(im);
+    div.appendChild(title);
+    header.appendChild(div);
+}
 
 // createHome();
 // createMenu();
